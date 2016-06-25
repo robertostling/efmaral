6a45b2c7704d064b334e6a835ba73e3628d74f95
Robert Östling <robert@ling.su.se>
2016-06-25 17:03:23 +0200

    Use mktemp in convenience script.

--- scripts/align_symmetrize.sh
+++ scripts/align_symmetrize.sh
@@ -16,9 +16,12 @@ SYMMETRIZATION=$4
 #if [ ! -z $4 ] ; then
 #    SYMMETRIZATION=$4
 #fi
-python3 efmaral.py --verbose -i "$1" "$2" "${@:5}" >align.forward &
-python3 efmaral.py --verbose -r -i "$1" "$2" "${@:5}" >align.back &
+
+FWD=`mktemp`
+BWD=`mktemp`
+python3 efmaral.py --verbose -i "$1" "$2" "${@:5}" >"$FWD" &
+python3 efmaral.py --verbose -r -i "$1" "$2" "${@:5}" >"$BWD" &
 wait
-atools -c $SYMMETRIZATION -i align.forward -j align.back >"$3"
-rm align.forward align.back
+atools -c $SYMMETRIZATION -i "$FWD" -j "$BWD" >"$3"
+rm "$FWD" "$BWD"
 
